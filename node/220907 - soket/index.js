0855c3ed83e96275bfb0e8d906a12fecd29b7824
memoring8 <memo091577@gmail.com>
2022-09-07 18:05:28 +0900

    study: socket

--- node/220907 - soket/index.js
+++ node/220907 - soket/index.js
@@ -8,18 +8,24 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-// io.on('connection) 는 클라이언트 1명과 연결될 때마다 실행
-// 콜백함수로 들어온 socket은 클라이언트 1명. 
-// io는 여러명의 socket에 대한 정보를 가지고 있어요. ( 연결된 클라이언트의 정보를 다 갖고 있다. )
+// id => 클라이언트와의 모든 연결을 갖고 있는 친구
+// socket => 클라이언트 한 명, socket.id 클라이언트를 구분하는 식별자
 io.on('connection', function(socket){
-    console.log('connected');
-    // socket.emit 을 한다는 거는 지금 연결된 클라이언트 1명에게 이벤트를 보내는 거 ( 개별 메세지 )
-    socket.emit('hi', 'server hello');
-    socket.on('click', function(data){
-        console.log('client click');
-        socket.emit('clickResponse', 'success');
-        // io.emit은 연결된 모든 클라이언트한테 이벤트를 보내는 거 ( 전체 공지 )
-        io.emit('clickResponse', 'io success');
+    console.log('connected : ', socket.id);
+
+    socket.emit('info', socket.id);
+    
+    io.emit('notice', socket.id + '님이 입장하셨습니다.');
+
+    socket.on('send',function(data){
+        // data = {id: ~~~, msg: ~~};
+        console.log('client message :', data.msg);
+        io.emit('newMessage', data);
+    });
+
+
+    socket.on('disconnect', function(){
+        io.emit('notice', socket.id + '님이 퇴장하셨습니다.')
     });
 });
 
