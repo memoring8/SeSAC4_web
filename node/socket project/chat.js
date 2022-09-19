17b0dbbd983e9dd501c397de837d79d7ebca05fd
memoring8 <memo091577@gmail.com>
2022-09-19 10:48:32 +0900

    socket

---
+++ node/socket project/chat.js
@@ -0,0 +1,51 @@
+var express = require("express");
+const { send } = require("process");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.get("/", function(req,res){
+    console.log("client");
+    res.sendFile( __dirname + "/chat.html" );
+});
+
+// io -> 클라이언트와의 모든 연결을 갖고 있는 친구
+// socket > 클라이언트 한 명. socket.id 클라이언트를 구분하는 식별자
+var list = {};
+io.on("connection", function(socket){
+    console.log( "connected : ", socket.id );
+    socket.on("info2", function(data){
+        list[socket.id] = data.nickname;
+        io.emit("notice", data.nickname + "님이 입장하셨습니다.");
+
+        io.emit('list', list);
+    });
+    // socket.emit("info", socket.id);
+    socket.on("send", function(data){
+        console.log( "client message : ", data.msg );
+        data["nickname"] = list[socket.id];
+
+        if( data.to == '전체') {
+            io.emit('newMessage', data);
+        } else {
+            data['is_dm'] = true;
+            // data.to ='2';
+            // list = {a:'1', b:'2', c:'3'};
+            let socketID = Object.keys(list).find((k) => {return list[k] === data.to;});
+            
+            // 특정한 한 사람한테 보낼 때, 사용
+            io.to(socketID).emit('newMessage', data);
+            // 내 자신한테도 보내기
+            socket.emit("newMessage", data);
+        }
+    });
+
+    socket.on("disconnect", function(){
+        io.emit("notice", list[socket.id] + "님이 퇴장하셨습니다.");
+        delete list[socket.id];
+    });
+});
+
+http.listen( 8000, function(){
+    console.log( "Server port : ", 8000 );
+});
